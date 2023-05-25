f54c4ac1854bad83bea80f2eab49e015566603af
Bruno Costa <70346383+bruno000014@users.noreply.github.com>
2023-05-25 18:41:20 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,7 +6,7 @@
 	$db_name = "db_locadora";
 
 
-	$conexao = pg_connect($servidor, $usuario, $senha, $db_name) or die('Banco de dados indisponível.');
+	$conexao = mysqli_connect($servidor, $usuario, $senha, $db_name) or die('Banco de dados indisponível.');
 
 	date_default_timezone_set("America/Manaus");	
 	
