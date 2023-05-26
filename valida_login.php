0f66481d0b7583a36a26ae898ee73d85327a1704
Bruno Costa <70346383+bruno000014@users.noreply.github.com>
2023-05-26 11:33:59 -0400

    Update valida_login.php

--- valida_login.php
+++ valida_login.php
@@ -10,7 +10,7 @@
 	
 		$sql_valida_login = mysqli_query("SELECT * FROM login WHERE nome_login = '".$nome_login."' AND senha_login = '".$senha_login."'");
 		
-		if(fetchall($sql_valida_login) > 0){
+		if(mysql_num_rows($sql_valida_login) > 0){
 	
 			$registros_login = mysqli_fetch_assoc($sql_valida_login);
 				 
