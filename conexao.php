90cd616899a59e50686ee0885184dd90c80094ef
Bruno Costa <70346383+bruno000014@users.noreply.github.com>
2023-05-25 23:12:54 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php //session_start();
-	$db = "mysql -hcontainers-us-west-122.railway.app -uroot -pIH17yp9oEYfqIdbXZVil --port 8069 --protocol=TCP railway";
+	
 	$servidor = "containers-us-west-122.railway.app";
 	$usuario = "root";
 	$senha = "IH17yp9oEYfqIdbXZVil";
 	$db_name = "railway";
 
 
-	$conexao = mysqli_connect($servidor, $usuario, $senha, $db_name, $db) or die('Banco de dados indisponível.');
+	$conexao = mysqli_connect($servidor, $usuario, $senha, $db_name) or die('Banco de dados indisponível.');
 
 	date_default_timezone_set("America/Manaus");	
 	
