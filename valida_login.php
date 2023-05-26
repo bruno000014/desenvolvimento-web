2580f00227baa7264b9d549c45c7a8b07feb08cb
Bruno Costa <70346383+bruno000014@users.noreply.github.com>
2023-05-26 01:42:00 -0400

    Update valida_login.php

--- valida_login.php
+++ valida_login.php
@@ -8,7 +8,7 @@
 		$nome_login = $_POST['nome_login'];
 		$senha_login = $_POST['senha_login'];
 	
-		$sql_valida_login = $dsh->query("SELECT * FROM login WHERE nome_login = '".$nome_login."' AND senha_login = '".$senha_login."'");
+		$sql_valida_login = $dsn->query("SELECT * FROM login WHERE nome_login = '".$nome_login."' AND senha_login = '".$senha_login."'");
 		
 		if(mysqli_num_rows($sql_valida_login) > 0){
 	
