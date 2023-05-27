c0725c12b2fcb915cc29ac12a4f2fa95e38f6880
Bruno Costa <70346383+bruno000014@users.noreply.github.com>
2023-05-27 01:01:10 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,9 @@
 <?php require('.env'); //session_start();
 
 
-	$mysqli = mysqli_init();
-	$mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-	$mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
-	$mysqli->close();
+	$conexao = mysqli_init();
+	$conexao->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+	$conexao->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
 
 	date_default_timezone_set("America/Manaus");
 	
