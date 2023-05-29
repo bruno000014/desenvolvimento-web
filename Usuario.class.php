532556c01157581a229974cb9981de5a9c928248
Bruno Costa <70346383+bruno000014@users.noreply.github.com>
2023-05-29 15:15:29 -0400

    Update Usuario.class.php

--- Usuario.class.php
+++ Usuario.class.php
@@ -5,7 +5,7 @@ class Usuario{
     public functions login($nome_login, $senha_login){
         global $pdo;
 
-        $sql = "SELECT * FROM login WHERE nome_login = :nome_login AND senha_login = :senha_login";
+        $sql = "SELECT * FROM login WHERE nome_login = '".$nome_login."' AND senha_login = '".$senha_login."'";
         $sql = $pdo->prepare($sql);
         $sql->bindValue("nome_login", $nome_login);
         $sql->bindValue("senha_login", $senha_login);
